441d31f30cb7e3a16aaaef78c411981e4090a993
Мыльцев Александр Михайлович <myltsev_a@tass.ru>
2022-07-18 11:20:41 +0300

    sso

--- demo-django/demo/urls.py
+++ demo-django/demo/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 from django.contrib import admin
 from .views import attrs, index, metadata
 admin.autodiscover()
 
 urlpatterns = [
-    path(r'^$', index, name='index'),
-    path(r'^attrs/$', attrs, name='attrs'),
-    path(r'^metadata/$', metadata, name='metadata'),
+    re_path(r'^$', index, name='index'),
+    re_path(r'^attrs/$', attrs, name='attrs'),
+    re_path(r'^metadata/$', metadata, name='metadata'),
 ]
